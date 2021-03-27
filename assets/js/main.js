2e5e3c99f9dcb00c52b4aa8a54d9c0bb6e59d9e1
Tirso Lecointere <tirsolecointere@gmail.com>
2021-03-27 19:57:40 -0400

    add dark mode functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
-console.log('js here!')
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggler = document.querySelector('#darkmode-toggler');
+
+const enableDarkMode = () => {
+    document.body.classList.add('dark');
+    localStorage.setItem('darkMode', 'enabled');
+    darkModeToggler.classList.add('isDark')
+}
+const disableDarkMode = () => {
+    document.body.classList.remove('dark');
+    localStorage.setItem('darkMode', null);
+    darkModeToggler.classList.remove('isDark')
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+darkModeToggler.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+})
